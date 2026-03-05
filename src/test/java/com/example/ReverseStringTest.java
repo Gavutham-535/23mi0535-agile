5d7e1acb97cff70cefd36267da81953f81843429
Blessy11-hub <blessiestanley@gmail.com>
2026-03-05 17:06:54 +0530

    23MIS0535 Gavutham

--- src/test/java/com/example/ReverseStringTest.java
+++ src/test/java/com/example/ReverseStringTest.java
@@ -1,11 +1,12 @@
-package com.exmaple;
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class ReverseStringTest {
     @Test
-    public static void testReverse() {
+    void testReverse() {
         ReverseString rs = new ReverseString();
         assertEquals("olleh", rs.reverse("hello"));
-        assertEquals("321", rs.reverse("123"));
-        assertEquals("", rs.reverse(""));
     }
 }
